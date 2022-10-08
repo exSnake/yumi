06b3669d640cdc8a4f25c363f672926a79577453
exSnake <giaqui@gmail.com>
2022-10-09 00:36:48 +0200

    Add input converter

---
+++ src/yumicore/lib/core/util/input_converter.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+
+import '../error/failures.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    try {
+      var result = int.parse(str);
+      if (result < 0) throw const FormatException();
+      return Right(result);
+    } on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {
+  @override
+  List<Object?> get props => [''];
+}
